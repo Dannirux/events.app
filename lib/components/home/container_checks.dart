9400fb5399f53d65df85a9b74465fd6337fcac95
Rodrigo Puente <rjpuente@uce.edu.ec>
2023-08-16 03:23:58 -0500

    Agregado de imagen de fondo y redireccion al widget de detalles

--- lib/components/home/container_checks.dart
+++ lib/components/home/container_checks.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:project_moviles/models/event.dart';
 
+import '../detail/place_detail_screen.dart';
+
 class ContainerChecks extends StatelessWidget {
   final List<Event> events;
 
@@ -16,42 +18,61 @@ class ContainerChecks extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         final event = events[index];
 
-        return Container(
-          padding: EdgeInsets.all(20),
-          margin: EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
-          width: 180,
-          height: 250,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 40,
-                height: 40,
-                margin: EdgeInsets.only(right: 10),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 123, 0, 245),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PlaceDetailScreen(
+                  event: event,
+                  screenHeight: MediaQuery.of(context).size.height,
                 ),
-                child: Icon(Icons.person, color: Colors.white),
               ),
-              Text(
-                event.name,
-                style: GoogleFonts.montserrat(color: Colors.white),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
+            width: 180,
+            height: 250,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  margin: EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 123, 0, 245),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Icon(Icons.person, color: Colors.white),
+                ),
+                Text(
+                  event.name,
+                  style: GoogleFonts.montserrat(color: Colors.white),
+                ),
+                Text(
+                  event.description,
+                  style:
+                      GoogleFonts.montserrat(color: Colors.white, fontSize: 15),
+                ),
+                Text(
+                  '${DateFormat.MMMd().format(DateTime.now())}',
+                  style:
+                      GoogleFonts.montserrat(color: Colors.white, fontSize: 15),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.lightBlue,
+              image: DecorationImage(
+                image: NetworkImage(event.images[0]),
+                fit: BoxFit.cover,
               ),
-              Text(
-                event.description,
-                style: GoogleFonts.montserrat(color: Colors.white, fontSize: 15),
-              ),
-              Text(
-                '${DateFormat.MMMd().format(DateTime.now())}',
-                style: GoogleFonts.montserrat(color: Colors.white, fontSize: 15),
-              ),
-            ],
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.0),
-            color: Colors.lightBlue,
+            ),
           ),
         );
       },
